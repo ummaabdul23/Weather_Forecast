4a3bd5c6d3aa09aaf9cc7791e50f5f2e5a0a640c
ummhaanii <ummaabdul23@gmail.com>
2025-01-08 08:46:48 +0100

    fix: update app.js to resolve api call issues and improve weather display functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //openWeatherMap API key
-const apiKey = "";
+const apiKey = "30d407abe5423f52cc08e17335c9e00f";
 
 //Function to get weather data
 const getWeather = async (city) => {
@@ -28,6 +28,7 @@ const displayWeather = (data) => {
     }
 
     const weatherContainer = document.getElementById("weather-container");
+    weatherContainer.style.display = "block";
     weatherContainer.innerHTML = `
         <h2>Weather in ${data.name}, ${data.sys.country}</h2>
         <p><strong>Temperature:</strong>${data.main.temp}°C</p>
@@ -51,6 +52,5 @@ form.addEventListener("submit", (e) => {
     }
 
 
-    const weatherData = getWeatherData(cityInput);
-    displayWeather(weatherData);
+    getWeather(cityInput).then(displayWeather);
 });
